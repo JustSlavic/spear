aac9c12d003953bccb88860a110a48b4709c334b
vradko <justslavic@gmail.com>
2023-09-22 13:29:43 +0400

    Remove test UI

--- code/game_platformer/game.cpp
+++ code/game_platformer/game.cpp
@@ -291,70 +291,8 @@ INITIALIZE_MEMORY_FUNCTION(initialize_memory)
         };
 
         ui::set_position(gs->hud, button_1, V2(100, 100));
-        ui::set_color(gs->hud, shape_1, V4(1, 0, 0, 1));
-
-        ui::animate(gs->hud, button_1, STRID("button_1.on_click.rotation"), UI_ANIM_ROTATION, 5, 0.f, 30.f);
-        ui::animate(gs->hud, button_1, STRID("button_1.on_release.rotation"), UI_ANIM_ROTATION, 5, 30.f, 0.f);
-
-        ui::animate(gs->hud, shape_1, STRID("shape_1.on_hover.color.r"), UI_ANIM_COLOR_R, 20, 1.f, 0.5f);
-        ui::animate(gs->hud, shape_1, STRID("shape_1.on_hover.color.g"), UI_ANIM_COLOR_G, 10, 0.f, 0.8f);
-        ui::animate(gs->hud, shape_1, STRID("shape_1.on_leave.color.r"), UI_ANIM_COLOR_R, 20, 0.5f, 1.f);
-        ui::animate(gs->hud, shape_1, STRID("shape_1.on_leave.color.g"), UI_ANIM_COLOR_G, 10, 0.8f, 0.f);
-
-        auto button_2 = ui::make_group(gs->hud);
-        ui::set_position(gs->hud, button_2, V2(500, 600));
-        ui::set_rotation(gs->hud, button_2, 20.f);
-
-        auto shape_2 = ui::make_shape(gs->hud, button_2);
-        ui::set_color(gs->hud, shape_2, V4(0.9, 0.4, 0.2, 1.0));
-        auto hover_callbacks_2 = ui::make_hoverable(gs->hud, button_2);
-        auto click_callbacks_2 = ui::make_clickable(gs->hud, button_2);
-
-        ui::animate(gs->hud, shape_2, STRID("shape_2.on_enter.color.g"), UI_ANIM_COLOR_G | UI_ANIM_PPONG, 40, 0.4f, 0.6f);
-        hover_callbacks_2->on_enter_internal = [](ui::system *s, ui::handle h)
-        {
-            ui::play_animation(s, "shape_2.on_enter.color.g");
-        };
-        ui::animate(gs->hud, shape_2, STRID("shape_2.on_leave.color.g"), UI_ANIM_COLOR_G, 40, 0.6f, 0.4f);
-        hover_callbacks_2->on_leave_internal = [](ui::system *s, ui::handle h)
-        {
-            ui::stop_animation(s, "shape_2.on_enter.color.g");
-            ui::play_animation(s, "shape_2.on_leave.color.g");
-        };
-
-        ui::animate(gs->hud, shape_2, STRID("shape_2.on_press.position.x"), UI_ANIM_POSITION_X, 3, 0, 5);
-        ui::animate(gs->hud, shape_2, STRID("shape_2.on_press.position.y"), UI_ANIM_POSITION_Y, 3, 0, 5);
-        click_callbacks_2->on_press_internal = [](ui::system *s, ui::handle h)
-        {
-            ui::play_animation(s, "shape_2.on_press.position.x");
-            ui::play_animation(s, "shape_2.on_press.position.y");
-        };
-        ui::animate(gs->hud, shape_2, STRID("shape_2.on_release.position.x"), UI_ANIM_POSITION_X, 3, 5, 0);
-        ui::animate(gs->hud, shape_2, STRID("shape_2.on_release.position.y"), UI_ANIM_POSITION_Y, 3, 5, 0);
-        click_callbacks_2->on_release_internal = [](ui::system *s, ui::handle h)
-        {
-            ui::play_animation(s, "shape_2.on_release.position.x");
-            ui::play_animation(s, "shape_2.on_release.position.y");
-        };
-
-        auto shape_3 = ui::make_shape(gs->hud);
-        ui::set_position(gs->hud, shape_3, V2(400, 200));
-        ui::set_scale(gs->hud, shape_3, V2(5, 5));
-        ui::set_rotation(gs->hud, shape_3, 70.f);
-        ui::set_color(gs->hud, shape_3, V4(0.3, 0.3, 0.8, 1.0));
-
-        ui::animate(gs->hud, shape_3, STRID("shape_3.ppong.position.x"), UI_ANIM_POSITION_X | UI_ANIM_PPONG, 120, 0, 100);
-        ui::play_animation(gs->hud, "shape_3.ppong.position.x");
-
-        auto hover_callbacks_3 = ui::make_hoverable(gs->hud, shape_3);
-        hover_callbacks_3->on_enter_internal = [](ui::system *s, ui::handle h)
-        {
-            ui::set_color(s, h, V4(1, 0, 0, 0));
-        };
-        hover_callbacks_3->on_leave_internal = [](ui::system *s, ui::handle h)
-        {
-            ui::set_color(s, h, V4(0.3, 0.3, 0.8, 1.0));
-        };
+        ui::set_color(gs->hud, shape_1, V4(0, 0, 0, 1));
+        ui::set_rotation(gs->hud, shape_1, 45.f);
     }
 }
 
