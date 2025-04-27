5676276624f7c6ebccbec932d17cd22ea750279e
vradko <justslavic@gmail.com>
2025-04-27 22:39:50 +0400

    Add debug graph

--- code/main_sdl_opengl.cpp
+++ code/main_sdl_opengl.cpp
@@ -150,6 +150,70 @@ void draw_platonic_solid(gpu_mesh mesh, shader s, vector4 c, matrix4 m, matrix4 
     }
 }
 
+void debug_draw_line(float32 posx, float32 posy, float32 width, float32 height,
+    vector4 color,
+    gpu_mesh *m,
+    shader *s)
+{
+    transform tm = transform::translate(-1.f, -1.f, 0.f)
+                 * transform::scale(2.f / current_client_width,
+                                    2.f / current_client_height,
+                                    1.f)
+                 * transform::translate_x(0.5f * width + posx)
+                 * transform::translate_y(0.5f * height + posy)
+                 * transform::scale(0.5f * width,
+                                    0.5f * height,
+                                    1.f);
+
+    glUseProgram(s->id);
+    s->uniform("u_model", to_matrix4(tm));
+    s->uniform("u_view", matrix4::identity());
+    s->uniform("u_projection", matrix4::identity());
+    s->uniform("u_color", color);
+
+    glBindVertexArray(m->vao);
+    glDrawElements(GL_TRIANGLES, m->count, GL_UNSIGNED_INT, NULL);
+}
+
+void debug_draw_memory_usage(int32 *usage_array, usize count,
+    int32 index, int32 max_value,
+    gpu_mesh *m, shader *s)
+{
+    transform tm_ui = transform::translate(-1.f, -1.f, 0.f)
+                    * transform::scale(2.f / current_client_width,
+                                       2.f / current_client_height,
+                                       1.f);
+    int32 frame_posy = 100;
+    int32 frame_height = 200;
+    int32 frame_width = current_client_width;
+    // Draw frame for debug graph
+    {
+        int32 width[4] = { 1, 1, frame_width, frame_width };
+        int32 height[4] = { frame_height, frame_height, 1, 1 };
+        int32 posx[4] = { 0, frame_width - 1, 0, 0 };
+        int32 posy[4] = { frame_posy, frame_posy, frame_posy, frame_posy + frame_height };
+
+        for (int i = 0; i < 4; i++)
+        {
+            debug_draw_line(posx[i], posy[i], width[i], height[i], V4(1.f), m, s);
+        }
+    }
+    // Draw usage graph
+    for (int i = 0; i < count; i++)
+    {
+        float32 col_width = ((float32) current_client_width - 2) / count;
+        float32 col_height = (float32) usage_array[(i + index) % count] / max_value;
+
+        float32 col_posx = (float32) col_width * i;
+        float32 col_posy = (float32) frame_posy + 1;
+
+        auto color = col_height > 1.0f ? V4(1.f, 0.f, 0.f, 1.f)
+                                       : V4(0.f, 1.f, 0.f, 1.f);
+        debug_draw_line(col_posx, col_posy, col_width, frame_height * col_height,
+            color, m, s);
+    }
+}
+
 int main()
 {
     current_client_width = 1200;
@@ -544,6 +608,17 @@ int main()
             }
         }
 #endif
+        static int32 memory_usage[256] = {};
+        static int32 memory_usage_index = 0;
+
+        int32 max_value = 90;
+        memory_usage[memory_usage_index] = rand() % 100;
+        memory_usage_index = (memory_usage_index + 1) % 256;
+
+        debug_draw_memory_usage(memory_usage, ARRAY_COUNT(memory_usage),
+            memory_usage_index, max_value,
+            &gpu_square, &shader_color);
+
 
         glBindFramebuffer(GL_FRAMEBUFFER, ui_framebuffer.framebuffer_id);
         glClearColor(0.f, 0.f, 0.f, 0.f);
